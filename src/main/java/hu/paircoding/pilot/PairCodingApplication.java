95214136ee1b413e4ae4c7d36db30e018a696275
talosati <talos.atanaz@gmail.com>
2021-06-24 19:07:54 +0200

    initial commit

---
+++ src/main/java/hu/paircoding/pilot/PairCodingApplication.java
@@ -0,0 +1,10 @@
+package hu.paircoding.pilot;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PairCodingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PairCodingApplication.class, args);
+    }
+}
